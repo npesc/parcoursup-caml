c11ddd100aa7b860e56b68893319264f4c45fc5a
npecs <ahmed@MacBook-Pro-de-Ahmed-3.local>
2021-11-24 19:26:15 +0100

    + fonction nouvelle_session + ajoute_candidat + ajoute_formation + type formation,etudiant,session

--- parcoursup/api.ml
+++ parcoursup/api.ml
@@ -4,22 +4,43 @@
 ┴  ┴ ┴┴└─└─┘└─┘└─┘┴└─└─┘└─┘┴   
  *)
 type etat = | EnCours | Close
-type session = {mutable etat: etat}
+type formation = {
+  nom: string;
+  mutable capacite: int;
+  mutable voeux: string list;
+}
+type etudiant = {
+  nom: string;
+  mutable voeux: (int option * string) list; (* (rang_repondeur * nom_formation) list *) 
+}
 
+type session = {
+  id_session: string;
+  mutable etat: etat;
+  mutable candidats: etudiant list;
+  mutable formations: formation list;
+}
 
-let nouvelle_session () = 
-  failwith "non implémenté"
+let nouvelle_session () = {
+  id_session=string_of_float (Unix.time ());
+  etat=EnCours;
+  candidats=[];
+  formations=[];
+  }
 
-let ajoute_candidat session ~nom_candidat = 
-  ignore session;
-  ignore nom_candidat;
-  failwith "non implémenté"
+let ajoute_candidat session ~nom_candidat =
+  match nom_candidat with
+  | None -> begin 
+    session.candidats <- {nom=string_of_int (List.length session.candidats);voeux=[]} :: session.candidats;
+    end
+  | Some s -> begin session.candidats <- {nom=s;voeux=[]} :: session.candidats end;;
 
 let ajoute_formation session ~nom_formation ~capacite =
-  ignore session;
-  ignore nom_formation;
-  ignore capacite;
-  failwith "non implémenté"
+  match nom_formation, capacite with
+  | Some nom_f, Some cap_f ->
+    session.formations <- {nom=nom_f;capacite=cap_f;voeux=[]} :: session.formations
+  | _ -> invalid_arg "Paramètres de formation manquants";;
+  
 
 let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
   ignore session;
