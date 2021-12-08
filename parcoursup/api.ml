6fc4cd8853ed6c564fdd7f68a1d94ff12ae50b6b
npecs <npecs@protonmail.com>
2021-12-08 03:25:12 +0100

    Update 0322 Ajout fonction consulte_voeux, consulte_proposition, renonce Ajout fonctions utilitaires Fix renonce_rang: cas où le répondeur automatique n'est pas activé Le programme passe les tests dune.

--- parcoursup/api.ml
+++ parcoursup/api.ml
@@ -4,12 +4,14 @@
 ┴  ┴ ┴┴└─└─┘└─┘└─┘┴└─└─┘└─┘┴   
  *)
 open Hashtbl;;
-type etat = | Config | Appel | Close
+type etat = | Config | Appel
 
 let get_option = function
 | Some x -> x
 | None -> raise (Invalid_argument "Option.get");;
-
+let rec ind x lst c = match lst with
+  | [] -> raise(Failure "Not Found")
+  | hd::tl -> if (hd=x) then c else ind x tl (c+1);;
 let rec remove x l = match l with
   | [] -> []
   | hd :: tl -> if x=hd then remove x tl else hd::remove x tl;;
@@ -22,7 +24,9 @@ type session = {
   (* (formation, capacite) Hashtable *)
   commissions : (string, string list) Hashtbl.t; 
   (* (formation, liste d'appel) Hashtbl.t *)
-  propositions: (string, string) Hashtbl.t; 
+  propositions: (string, string list) Hashtbl.t; 
+  (* formation, fonction_comparaison *)
+  algo_locaux: (string, (string -> string -> int)) Hashtbl.t;
 }
 
 let nouvelle_session () = {
@@ -31,6 +35,7 @@ let nouvelle_session () = {
   formations=Hashtbl.create 100 ~random:false;
   commissions=Hashtbl.create 10 ~random:false;
   propositions=Hashtbl.create 100 ~random:false;
+  algo_locaux=Hashtbl.create 100 ~random:false;
   }
 
 let ajoute_candidat session ~nom_candidat = 
@@ -39,7 +44,8 @@ let ajoute_candidat session ~nom_candidat =
 ;;
 
 let ajoute_formation session ~nom_formation ~capacite =
-  Hashtbl.add session.formations nom_formation capacite;;
+  Hashtbl.add session.formations nom_formation capacite;
+  Hashtbl.add session.propositions nom_formation [];;
   
 
 let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
@@ -50,73 +56,83 @@ let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation =
   Hashtbl.replace (Hashtbl.find session.candidats nom_candidat) nom_formation (Some !rang)
 
 let ajoute_commission session ~nom_formation ~fonction_comparaison = 
-  let table_candidats = session.candidats in
-  let pretendants =  ref [] in
-
-  Hashtbl.iter (fun nom_c voeux -> (* voeux -> hashtable de voeux du candidat *)
-    if Hashtbl.mem voeux nom_formation 
-    then pretendants :=  nom_c :: !pretendants)
-    table_candidats;
-  (* on récupère tous les candidats qui souhaitent intégrer nom_formation 
-  et on les ajoute à la liste pretendants *)
-  ignore(fonction_comparaison);
-  pretendants := List.sort compare !pretendants;
-  (* on tri avec notre fonction de comparaison éventuellement compare dans ce cas*)
-  Hashtbl.add session.commissions nom_formation !pretendants;; 
-  (* on ajoute la liste triée aux commissions *)
+  let aux candidat1 candidat2 = match fonction_comparaison ~candidat1 ~candidat2 with | true -> 1 | _ -> -1 in
+  Hashtbl.replace session.algo_locaux nom_formation aux;;
 
 let renonce_rang rang_prop table_voeux candidat session =
   Hashtbl.iter (fun voeu rang ->
-        if ((get_option rang_prop) < (get_option rang)) then begin
-          (* si rang_proposition est inferieur à rang voeu courant *)
-          let cap_bis = Hashtbl.find session.formations voeu in
-          if (List.mem candidat (Hashtbl.find_all session.propositions voeu)) then begin
-          (* si candidat a reçu une proposition pour le voeu courant *)
-            Hashtbl.replace session.formations voeu (cap_bis+1);
-            Hashtbl.remove session.propositions voeu; 
-          end;
-          Hashtbl.remove table_voeux voeu;
-          Hashtbl.replace session.commissions voeu (remove candidat (Hashtbl.find session.commissions voeu));
-          (* suppression du candidat de la liste d'appel du voeu renoncé *)
-        end) table_voeux;;
+      if ((get_option rang_prop) < (get_option rang)) then begin
+      (* si rang_proposition est inferieur à rang voeu courant *)
+        let cap_bis = Hashtbl.find session.formations voeu in
+        if ((rang <> None)) then begin
+        (* si candidat a reçu une proposition pour le voeu courant et qu'il a activé son répondeur *)
+          Hashtbl.replace session.formations voeu (cap_bis+1);
+        end;
+  
+        if (List.length (Hashtbl.find session.propositions voeu) > 0) then
+          Hashtbl.replace session.propositions voeu (remove candidat (Hashtbl.find session.propositions voeu));
+        Hashtbl.remove table_voeux voeu;
+        Hashtbl.replace session.commissions voeu (remove candidat (Hashtbl.find session.commissions voeu)); 
+      (* suppression du candidat de la liste d'appel du voeu renoncé *)
+      end) table_voeux;;
 
 let reunit_commissions session =
-  session.etat <- Appel;;
+  session.etat <- Appel;
+  let table_candidats = session.candidats in
+  Hashtbl.iter (fun nom_c voeux -> (* voeux -> hashtable de voeux du candidat *)
+    Hashtbl.iter (fun voeu _ -> 
+      let commission = ref [] in
+        begin match (Hashtbl.find_opt session.commissions voeu) with
+        | None -> () | Some l -> commission := l; end; 
+      if not(List.mem nom_c !commission) 
+      then Hashtbl.replace session.commissions voeu (nom_c :: !commission)) voeux) table_candidats;
+  Hashtbl.iter (fun formation com -> 
+    Hashtbl.replace session.commissions formation (List.rev (List.sort (Hashtbl.find session.algo_locaux formation) com)))
+    session.commissions;;
+  (* on ajoute la liste triée aux commissions *)
 
 let nouveau_jour session =
   let rec propose f = function
     | [] -> (); (* liste d'appel vide -> rien proposer *)
     | x :: t ->
-        if (not(List.mem x (Hashtbl.find_all session.propositions f)) && (Hashtbl.find session.formations f > 0)) then begin
-          Hashtbl.add session.propositions f x; (* proposer au mieux classé *)
-          Hashtbl.replace session.commissions f t; (* on met à jour la liste d'appel *)
+        if (not(List.mem x (Hashtbl.find session.propositions f)) && (Hashtbl.find session.formations f > 0)) then begin
+          let prop_list = Hashtbl.find session.propositions f in
+          Hashtbl.replace session.propositions f (x::prop_list); (* proposer au mieux classé *)
+          Hashtbl.replace session.commissions f t; (* on met à jour la liste d'appel *) 
           let capacite_temp = Hashtbl.find session.formations f in
           Hashtbl.replace session.formations f (capacite_temp-1); (* on met à jour la capacité *)  
-        end;
-        propose f t;in
+        end; propose f t;in
   Hashtbl.iter (fun formation liste_appel ->
       propose formation liste_appel; 
       if (Hashtbl.length session.propositions > 0) then begin (* si ensemble propositions non vide *)
-        Hashtbl.iter (fun proposition candidat -> (* on itère sur les propositions*)
-            let voeux_candidat = (Hashtbl.find session.candidats candidat) in
-            if (Hashtbl.mem voeux_candidat proposition) then begin (* si proposition est dans les voeu du candidat *)
-              let rang_proposition = Hashtbl.find voeux_candidat proposition in
-              renonce_rang rang_proposition voeux_candidat candidat session;end) (* renonce au voeux de rang inférieur *)
-              session.propositions end) session.commissions;;
+        Hashtbl.iter (fun proposition liste_candidats -> (* on itère sur les propositions*)
+            List.iter (fun candidat -> 
+                let voeux_candidat = (Hashtbl.find session.candidats candidat) in 
+                let rang_proposition = Hashtbl.find voeux_candidat proposition in
+                renonce_rang rang_proposition voeux_candidat candidat session) liste_candidats) (* renonce aux voeux de rang inférieur *)
+          session.propositions end) session.commissions;;
 
 let renonce session ~nom_candidat ~nom_formation = 
-  (* Hashtbl.filter_map_inplace (fun nom v -> match v with |  -> None | _ -> Some v) session.propositions ;; *)
-  ignore session;
-  ignore nom_candidat;
-  ignore nom_formation;
-  failwith "non implémenté"
+  let prop_temp = Hashtbl.find session.propositions nom_formation in 
+  if (List.mem nom_candidat prop_temp) then begin
+    let capacite_temp = Hashtbl.find session.formations nom_formation in
+    Hashtbl.replace session.propositions nom_formation (remove nom_candidat prop_temp);
+    Hashtbl.replace session.formations nom_formation (capacite_temp+1) end;
+  Hashtbl.remove (Hashtbl.find session.candidats nom_candidat) nom_formation;(* suppression du voeu dans la tablevoeux du candidat *) 
+  let liste_appel = Hashtbl.find session.commissions nom_formation in
+  Hashtbl.replace session.commissions nom_formation (remove nom_candidat liste_appel);;
 
 let consulte_propositions session ~nom_candidat =
-  ignore session;
-  ignore nom_candidat;
-  failwith "non implémenté"
+  let liste = ref [] in
+  Hashtbl.iter (fun formation propositions -> 
+      if (List.mem nom_candidat propositions) then liste := formation :: !liste)
+    session.propositions;
+  List.rev !liste;;
 
 let consulte_voeux_en_attente session ~nom_candidat = 
-  ignore session;
-  ignore nom_candidat;
-  failwith "non implémenté"
+  let res = ref [] in
+  Hashtbl.iter (fun voeu _ ->
+      if (not(List.mem nom_candidat (Hashtbl.find session.propositions voeu)))
+      then res := (voeu, ind nom_candidat (Hashtbl.find session.commissions voeu) 0) :: !res;)
+    (Hashtbl.find session.candidats nom_candidat);
+  !res;;
